9bce75ac0c0c923da900d35de2cd528c2f2c4da4
samanthaaugust8231 <147224997+samanthaaugust8231@users.noreply.github.com>
2024-02-14 20:44:19 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -74,6 +74,8 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
+      message = connectionSocket.recv(1024)
+      filename = message.split()[1]
       outputdata = b"HTTP/1.1 400 Not Found\r\nServer: Apache\r\n Content-Type: text/html; charset=UTF-8\r\nKeep-Alive: timeout=5, max=1000\r\nConnection: Keep-Alive\r\n\r\n"
       connectionSocket.sendall(outputdata)
       
